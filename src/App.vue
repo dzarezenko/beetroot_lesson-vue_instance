bb9bee93dbb3467d519de62efd1c682baf82f848
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2020-01-11 12:02:54 +0200

    1st component created (1st Vue instance)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="app">
-    
+    <h1>Counter: {{ counter }}</h1>
+    <button @click="increaseCounter">Increase</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
+  data() {
+    return {
+      counter: 0,
+    }
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++;
+    }
+  },
 }
 </script>
