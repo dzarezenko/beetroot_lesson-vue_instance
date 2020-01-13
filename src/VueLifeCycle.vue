b46137be2592b8924c330f6326571d80a94cd97f
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2020-01-13 20:18:48 +0200

    Vue Component Lifecycle

---
+++ src/VueLifeCycle.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <button @click="changeTitle">Change Title</button>
+    <button @click="destroy">Kill</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Some title",
+    }
+  },
+
+  beforeCreate() {
+    console.log("[VUE] beforeCreate()");
+  },
+  created() {
+    console.log("[VUE] created()");
+  },
+
+  beforeMount() {
+    console.log("[VUE] beforeMount()");
+  },
+  mounted() {
+    console.log("[VUE] mounted()");
+  },
+
+  beforeUpdate() {
+    console.log("[VUE] beforeUpdate()");
+  },
+  updated() {
+    console.log("[VUE] updated()");
+  },
+
+  beforeDestroy() {
+    console.log("[VUE] beforeDestroy()");
+  },
+  destroyed() {
+    console.log("[VUE] destroyed()");
+  },
+
+  methods: {
+    changeTitle() {
+      //alert("call!");
+      this.title+= "a";//"Some New Title";
+    },
+    destroy() {
+      this.$destroy();
+    },
+  },
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './VueLifeCycle.vue'
 
 Vue.config.productionTip = false
-
-Vue.component("hello", {
-  template: "<h1>Hello</h1>",
-});
 
 var vm = new Vue({
   render: function (h) { return h(App) },
@@ -15,18 +11,4 @@ var vm = new Vue({
 // Some logic
 
 vm.$mount('#app');
-console.log(vm);
 
-var vm2 = new Vue({
-  render: function (h) { return h(App) },
-  //el: "#app",
-})
-
-// Some logic
-
-//vm2.$mount('#app2');
-
-/*var vm3 = new Vue({
-  el: "#app2",
-  template: "<p>Some text</p>"
-})*/
